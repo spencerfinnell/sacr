52baeb880a2ac8f31b93625e819ea65ace11f5cc
Spencer Finnell <spencerfinnell@gmail.com>
2013-09-04 19:42:05 -0400

    Single day headlines. Closes #21

--- inc/timeline/timeline.php
+++ inc/timeline/timeline.php
@@ -8,3 +8,13 @@ function sacr_post_type_time_period() {
 	$time_period->register_taxonomy( 'Location', array( 'hierarchical' => true ) );
 }
 add_action( 'init', 'sacr_post_type_time_period' );
+
+function sacr_timeline_regional( $atts, $content ) {
+	return '<h3 class="timeline-item-title regional">' . $content . '</h3>';
+}
+add_shortcode( 'timeline-regional', 'sacr_timeline_regional', 10, 2 );
+
+function sacr_timeline_local( $atts, $content ) {
+	return '<h3 class="timeline-item-title local">' . $content . '</h3>';
+}
+add_shortcode( 'timeline-local', 'sacr_timeline_local', 10, 2 );
